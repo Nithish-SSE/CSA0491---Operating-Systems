d04f10b8356ff077057ef54828820345fb22e477
Nithish-SSE <113259370+Nithish-SSE@users.noreply.github.com>
2023-01-09 21:36:22 +0530

    Update Program 1.c

--- Program 1.c
+++ Program 1.c
@@ -1,22 +1,24 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
-#include<sys/types.h>
 int main()
 {
-pid_t p;
-printf("before fork\n");
-p=fork();
-if(p==0)
-{
-printf("I am  having student id %d\n",getpid());
-printf("My parent's id is %d\n",getppid());
-}
-else{
-printf("My student's id is %d\n",p);
-printf("I am having parent id %d\n",getpid());
-}
-printf("Common\n");
+    pid_t pid, ppid;
+
+    pid = fork();
+    if (pid == 0)
+    {
+        pid = getpid();
+        ppid = getppid();
+        printf("Child process: PID = %d, PPID = %d\n", pid, ppid);
+    }
+    else
+    {
+        pid = getpid();
+        printf("Parent process: PID = %d\n", pid);
+    }
+
+    return 0;
 }
 
 
