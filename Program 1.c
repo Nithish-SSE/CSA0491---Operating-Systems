c2d614e6d821d0d17914c6ae29001c0ceaa1b955
Nithish-SSE <113259370+Nithish-SSE@users.noreply.github.com>
2023-01-09 21:46:34 +0530

    Update Program 1.c

--- Program 1.c
+++ Program 1.c
@@ -5,8 +5,8 @@ int main()
 {
     int p_id, p_pid;
 
-    p_id = getpid(); /process id/
-    p_pid = getpid(); /parent process id/
+    p_id = getpid();
+    p_pid = getpid();
 
     printf("Process ID: %d\n", p_id);
     printf("Parent Process ID: %d\n", p_pid);
